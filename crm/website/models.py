1b8911d63830519fdb5f3b7b23b2de583005040f
Vitek88 <witalij.duszejko@gmail.com>
2025-07-10 12:29:58 +0200

    some functionality added

--- crm/website/models.py
+++ crm/website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.CharField(max_length=150, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=50, blank=True, null=True)
+    zipcode = models.CharField(max_length=10, blank=True, null=True, help_text="Postal code")
+
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Record creation timestamp")
+    updated_at = models.DateTimeField(auto_now=True, help_text="Record last update timestamp")
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name if self.first_name and self.last_name else 'Unnamed Record'
--- crm/website/views.py
+++ crm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -24,4 +25,23 @@ def home(request):
 #     pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, f'User has been logged out.')
+    return redirect('home')  # Redirect to home or another page after logout
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            #Authenticate the user after registration and login
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
